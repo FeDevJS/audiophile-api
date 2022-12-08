950b2f0a08e417e19e891e01d01355ce9faa0b5e
Fprog <fnaive@hotmail.com>
2022-12-07 21:12:43 -0300

    fix import speakers route

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const headphones = require('./categories/headphones');
+const speakers = require('../routes/categories/speakers');
 
 router.use( headphones );
+router.use( speakers );
 
 module.exports = router;
